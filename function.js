827952301a23c479d851b93546487aebf862de8a
Hani Abu Zeid <31956031+swedenSnow@users.noreply.github.com>
2018-01-29 16:06:24 +0100

    Added html5shiv

---
+++ function.js
@@ -0,0 +1,49 @@
+// Function for hamburger meny & scroll up function
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+   if (x.className === "topnav") {
+       x.className += " responsive";
+    } else {
+       x.className = "topnav";
+   }
+}
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event) {
+ didScroll = true;
+});
+
+setInterval(function() {
+ if (didScroll) {
+   hasScrolled();
+   didScroll = false;
+ }
+}, 250);
+
+function hasScrolled() {
+ var st = $(this).scrollTop();
+
+ // Make sure they scroll more than delta
+ if (Math.abs(lastScrollTop - st) <= delta)
+   return;
+
+ // If they scrolled down and are past the navbar, add class .nav-up.
+ // This is necessary so you never see what is "behind" the navbar.
+ if (st > lastScrollTop && st > navbarHeight) {
+   // Scroll Down
+   $('nav').removeClass('topnav').addClass('nav-up');
+ } else {
+   // Scroll Up
+   if (st + $(window).height() < $(document).height()) {
+     $('nav').removeClass('nav-up').addClass('topnav');
+   }
+ }
+
+ lastScrollTop = st;
+}
